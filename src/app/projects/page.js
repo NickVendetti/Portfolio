ad4c27fbe533aa60b764bc88a62822bef88d8b39
NickVendetti <nrvendetti@gmail.com>
2025-09-25 15:52:16 -0400

    fixed function for live demo

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -6,7 +6,7 @@ export default function Projects() {
         "A platform for discovering photographers based on style and location.",
       techStack: ["React", "Express", "PostgreSQL", "Node.js", "TailwindCSS"],
       github: "https://github.com/NickVendetti/photo-finder",
-      Site: "https://photo-finder-ten.vercel.app/"
+      demo: "https://photo-finder-ten.vercel.app/"
     },
     {
       name: "Jobly",
